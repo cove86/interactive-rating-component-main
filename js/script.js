4b664beaa5ebf3a345523eccd3877c7b04491a24
Ashley Cove <ash.cove99@gmail.com>
2022-05-04 08:10:23 +0100

    Added selectors to js, eventlisteners to rating and submit btns

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-"use strict";
+'use strict';
+
+const ratingBtnDiv = document.querySelector('.rating-div');
+const ratingBtn = document.querySelectorAll('.rating-number');
+const submitBtn = document.querySelector('.btn-submit');
+const ratingCard = document.getElementById('rating-card');
+const thankYouCard = document.getElementById('thankyou-card');
+let ratingText = document.querySelector('.rating-text').innerHTML;
+
+let rating;
+
+ratingBtnDiv.addEventListener('click', e => {
+  let clicked = e.target.closest('.rating-number');
+
+  if (!clicked) return;
+
+  ratingBtn.forEach(b => b.classList.remove('rating-selected'));
+
+  clicked.classList.add('rating-selected');
+
+  rating = clicked.dataset.src;
+});
+
+submitBtn.addEventListener('click', e => {
+  console.log(rating);
+  ratingCard.classList.add('hide');
+  ratingText = `You selected ${rating} out of 5`;
+  thankYouCard.classList.remove('hide');
+});
